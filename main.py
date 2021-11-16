01a7d57a74ebf87a5a70c3a0050c0dbbaca6b471
Ricardo Quintela <69524535+FiFcreeN@users.noreply.github.com>
2021-11-16 13:35:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from aux_funcs import *
-
-def readText(filename: str):
-    """
-    Reads the information from a file and stores it in an array\n
-
-    Args:
-        filename: the path of the file to be read
-
-    Returns:
-        an array with the read information
-    """
-
-    with open(filename, "rb") as f:
-        return np.array(list(f.read()))
+import sys
 
 
+def main(args):
 
-def main():
-    file = readText("project_files\\dataset\\bible.txt")
-    print(entropia(file, np.arange(0, 2**8)))
+    #get the file name
+    if len(args) < 2:
+        filename = input("Escreva o nome do ficheiro>>>")
+    elif len(args) > 2:
+        return
+    else:
+        filename = args[1]
+
+    #calculate entropy and read the graph
+    try:
+        file = readText("%s" % (filename))
+    except:
+        return
+
+    saveData(filename, "------------------\n%s\nEntropia: %s" % (filename, entropia(file, np.arange(0, 2**8))))
+    anlzFt(file, np.arange(0, 2**8), filename)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
